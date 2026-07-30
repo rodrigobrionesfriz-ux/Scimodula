41ee40ce3e9f58904fc5ac86d37ca74328150c41
rodrigobrionesfriz-ux <rodrigo.briones.friz@gmail.com>
2026-07-30 18:00:15 -0400

    Update service worker cache version to v107

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    de index.html y el SW no registraba). Estrategia: cache-first con
    precache versionado; la red actualiza el cache en segundo plano. */
 
-const CACHE = 'sci-v106';
+const CACHE = 'sci-v107';
 
 const ASSETS = [
   './',
